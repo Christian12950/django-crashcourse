2f4ab56970a6d07fe9e7ec71f3fd23ea358dc1b2
Christian Brand <christian12950@gmail.com>
2018-08-14 12:36:33 -0400

    word counter progress

---
+++ wordCount/wordCount/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+
+def home(request):
+    return render(request, 'home.html', {'header': 'Word Count'})
+
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    print(fulltext)
+    wordList = fulltext.split()
+    wordDictionary = {}
+
+    for word in wordList:
+        if word in wordDictionary:
+            wordDictionary[word] += 1
+        else:
+            wordDictionary[word] = 1
+
+    sortedWords = sorted(wordDictionary.items(), key=operator.itemgetter(1), reverse=True)
+    print(wordDictionary)
+    return render(request, 'count.html', {'fulltext': fulltext, 'wordcount': len(wordList), 'sortedWords': sortedWords})
+

a7a7740d4e37fd1b85810905bc472519f3963876
Christian Brand <christian12950@gmail.com>
2018-08-19 12:32:20 -0400

    new project

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\chris\onedrive\documents\github\django-crashcourse\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

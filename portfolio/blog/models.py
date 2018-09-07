5bc676690ac0d8ab9c14de1ab73d9f2cee05cfe4
Christian Brand <christian12950@gmail.com>
2018-09-06 22:18:59 -0400

    updating blog

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    def __self__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_formatted(self):
+        return self.pub_date.strftime('%b %e, %Y')
+
